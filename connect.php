0fb92c8b24d821428672a3309c3526c8449fb062
Abhinav <in.abhi9@gmail.com>
2014-04-19 02:24:27 +0530

    change url

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ include 'connectors/GdriveConnector.php';
 
 $appKey = '2zy6r4a4ycfpfrv';
 $appSecret = '39x02dv0vhzpqc5';
-$rU = 'http://localhost/playground/zapstich-demo/connect.php?service=dropbox';
+$rU = 'http://localhost/playground/zapstitch-demo/connect.php?service=dropbox';
 
 $service = $_GET['service'];
 
@@ -21,7 +21,7 @@ if ($service == "dropbox"){
 } else if ($service == "gdrive"){
 	$clientId = "164477918055.apps.googleusercontent.com";
 	$clientSecret = "xv4GumtfKL5eZ9yDKAG11042";
-	$rU = 'http://localhost/playground/zapstich-demo/connect.php?service=gdrive';
+	$rU = 'http://localhost/playground/zapstitch-demo/connect.php?service=gdrive';
 	$gdriveConnector = new GdriveConnector($clientId, $clientSecret, $rU);
 	try{
 		$gdriveConnector->connectFinish();
