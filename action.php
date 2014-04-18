5f96fe20e77e271dcfa71ea0eb170ffcc800b2f9
Abhinav <in.abhi9@gmail.com>
2014-04-19 02:24:16 +0530

    experimenting action is now implemented

--- action.php
+++ action.php
@@ -5,6 +5,23 @@ include 'connectors/GdriveConnector.php';
 
 
 $gdriveConnector = new GdriveConnector(null, null, null);
+$dropboxConnector = new DropboxConnector(null, null, null, 'zapstitch-demo/1.0');
+$isFileFound = false;
+$dirToLookup = '/Public'; // browse this directory to get a file
 
-//$gdriveConnector->uploadFile('/tmp/a.json', '/');
-$gdriveConnector->createBlankFile('nofile.cpp', '');
+// Download file if found in user's home directory
+foreach ($dropboxConnector->listFolders($dirToLookup) as $item){
+	if ($item['is_dir'] != 1){
+		$out = $dropboxConnector->downloadFile($item['path'], '/tmp');
+		$isFileFound = true;
+		break;
+	}
+}
+
+// uploading file if found else create noname blank file
+if ($isFileFound == true){
+	$uploadName = explode('.n3w.', $out);
+	$gdriveConnector->uploadFile($out, basename($uploadName[0]));
+} else {
+	$gdriveConnector->createBlankFile('noname.cpp', '');
+}
